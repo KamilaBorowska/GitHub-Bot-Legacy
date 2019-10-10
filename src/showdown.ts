5708a6410a24d85af7d878fcec7c765314f73268
Konrad Borowski <konrad@borowski.pw>
2019-10-10 20:51:50 +0200

    Use ES6 class for Showdown

--- src/showdown.ts
+++ src/showdown.ts
@@ -4,7 +4,6 @@ var MINUTE = 60000
 
 var EventEmitter = require('events')
 
-var format = require('python-format')
 var WebSocketClient = require('websocket').client
 
 var request = require('request')
@@ -18,135 +17,133 @@ function copySafely (target, configuration, keys) {
   for (var key in configuration) {
     if (!objectHas(configuration, key)) return
     if (!objectHas(keys, key)) {
-      throw new TypeError(format("Unrecognized key '{}'", key))
+      throw new TypeError(`Unrecognized key '${key}'`)
     }
     target[key] = configuration[key]
     added += 1
   }
   if (added !== Object.keys(keys).length) {
-    var message = format('Expected {} keys, got {}.', keys.size, added)
-    throw new TypeError(message)
+    throw new TypeError(`Expected ${keys.size} keys, got ${added}.`)
   }
 }
 
-function Showdown (configuration) {
-  this.queue = Promise.resolve()
-  copySafely(this, configuration, Showdown.keys)
-}
+class Showdown extends EventEmitter {
+  queue: Promise<void>
 
-Showdown.keys = {
-  server: true,
-  serverport: true,
-  serverid: true,
-  nickname: true,
-  password: true,
-  room: true
-}
-
-Showdown.commands = new Map([
-  ['challstr', 'finalize'],
-  ['c:', 'onChatMessage']
-])
-
-Showdown.prototype = new EventEmitter()
-
-Showdown.prototype.connect = function connect () {
-  var connection = new WebSocketClient()
-  connection.on('connectFailed', this.onConnectionFailure.bind(this))
-  connection.on('connect', this.onConnect.bind(this))
-
-  var connectionString = this.getConnectionString()
-  connection.connect(connectionString, [])
-}
-
-Showdown.prototype.getConnectionString = function getConnectionString () {
-  return format(
-    'ws://{}:{}/showdown/websocket',
-    this.server,
-    this.serverport
-  )
-}
-
-Showdown.prototype.onConnectionFailure = function onConnectionFailure (error) {
-  console.error('Error occured (%s), will connect in a minute', error)
-  setTimeout(this.connect.bind(this), MINUTE)
-}
-
-Showdown.prototype.onConnect = function onConnect (connection) {
-  this.connection = connection
-
-  var onConnectionFailure = this.onConnectionFailure.bind(this)
-  connection.on('error', onConnectionFailure)
-  connection.on('close', onConnectionFailure)
-  connection.on('message', this.onMessage.bind(this))
-  console.info('Connected to Showdown server')
-}
-
-Showdown.prototype.onMessage = function onMessage (message) {
-  if (message.type !== 'utf8') return
-  this.parseMessage(message.utf8Data)
-}
-
-Showdown.prototype.parseMessage = function parseMessage (message) {
-  console.log(message)
-  var parts = message.split('|')
-  if (Showdown.commands.has(parts[1])) {
-    this[Showdown.commands.get(parts[1])](parts)
+  constructor(configuration) {
+    super()
+    this.queue = Promise.resolve()
+    copySafely(this, configuration, Showdown.keys)
   }
-}
 
-Showdown.prototype.finalize = function finalize (parts) {
-  var id = parts[2]
-  var str = parts[3]
-  var nickname = this.nickname
-  var password = this.password
-  request.post(
-    format('https://play.pokemonshowdown.com/~~{}/action.php', this.serverid),
-    {
-      form: {
-        act: 'login',
-        challengekeyid: id,
-        challenge: str,
-        name: nickname,
-        pass: password
-      }
-    },
-    function finish (error, response, body) {
-      if (error) {
-        this.finalize(parts)
-        return
-      }
-      var result = JSON.parse(body.replace(/^]/, ''))
-      var assertion = result.assertion
-      var command = format('/trn {},0,{}', nickname, assertion)
-      this.report(command)
-      this.report('/join ' + this.room)
-      this.report('/join staff')
-      this.report('/away')
-    }.bind(this)
-  )
-}
+  static keys = {
+    server: true,
+    serverport: true,
+    serverid: true,
+    nickname: true,
+    password: true,
+    room: true
+  }
 
-Showdown.prototype.onChatMessage = function onChatMessage (parts: string[]) {
-  this.emit('message', parts[3], parts.slice(4).join('|'))
-}
+  static commands = new Map([
+    ['challstr', 'finalize'],
+    ['c:', 'onChatMessage']
+  ])
 
-Showdown.prototype.report = function report (message: string) {
-  this.queue = this.queue.then(() => {
-    this.connection.send(`${this.room}|${message}`.replace(/\n/g, ''))
-    return new Promise(resolve => {
-      setTimeout(resolve, 500)
+  connect() {
+    var connection = new WebSocketClient()
+    connection.on('connectFailed', this.onConnectionFailure.bind(this))
+    connection.on('connect', this.onConnect.bind(this))
+
+    var connectionString = this.getConnectionString()
+    connection.connect(connectionString, [])
+  }
+
+  getConnectionString() {
+    return `ws://${this.server}:${this.serverport}/showdown/websocket`
+  }
+
+  onConnectionFailure(error) {
+    console.error('Error occured (%s), will connect in a minute', error)
+    setTimeout(this.connect.bind(this), MINUTE)
+  }
+
+  onConnect(connection) {
+    this.connection = connection
+
+    var onConnectionFailure = this.onConnectionFailure.bind(this)
+    connection.on('error', onConnectionFailure)
+    connection.on('close', onConnectionFailure)
+    connection.on('message', this.onMessage.bind(this))
+    console.info('Connected to Showdown server')
+  }
+
+  onMessage(message) {
+    if (message.type !== 'utf8') return
+    this.parseMessage(message.utf8Data)
+  }
+
+  parseMessage(message) {
+    console.log(message)
+    var parts = message.split('|')
+    if (Showdown.commands.has(parts[1])) {
+      this[Showdown.commands.get(parts[1])](parts)
+    }
+  }
+
+  finalize (parts) {
+    var id = parts[2]
+    var str = parts[3]
+    var nickname = this.nickname
+    var password = this.password
+    request.post(
+      `https://play.pokemonshowdown.com/~~${this.serverId}/action.php`,
+      {
+        form: {
+          act: 'login',
+          challengekeyid: id,
+          challenge: str,
+          name: nickname,
+          pass: password
+        }
+      },
+      function finish (error, response, body) {
+        if (error) {
+          this.finalize(parts)
+          return
+        }
+        var result = JSON.parse(body.replace(/^]/, ''))
+        var assertion = result.assertion
+        var command = `/trn ${nickname},0,${assertion}`
+        this.report(command)
+        this.report('/join ' + this.room)
+        this.report('/join staff')
+        this.report('/away')
+      }.bind(this)
+    )
+  }
+
+  onChatMessage(parts: string[]) {
+    this.emit('message', parts[3], parts.slice(4).join('|'))
+  }
+
+  report(message: string) {
+    this.queue = this.queue.then(() => {
+      this.connection.send(`${this.room}|${message}`.replace(/\n/g, ''))
+      return new Promise(resolve => {
+        setTimeout(resolve, 500)
+      })
     })
-  })
-}
+  }
 
-Showdown.prototype.reportStaff = function report (message: string) {
-  this.queue = this.queue.then(() => {
-    this.connection.send(`staff|${message}`.replace(/\n/g, ''))
-    return new Promise(resolve => {
-      setTimeout(resolve, 500)
+  reportStaff(message: string) {
+    this.queue = this.queue.then(() => {
+      this.connection.send(`staff|${message}`.replace(/\n/g, ''))
+      return new Promise(resolve => {
+        setTimeout(resolve, 500)
+      })
     })
-  })
+  }
 }
 
 module.exports = Showdown
